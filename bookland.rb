424a94f8dcc994e43b63cbde80ddf3fdb67dd76f
Tore Darell <toredarell@gmail.com>
2008-02-28 23:44:49 +0100

    Added Bookland barcodes

---
+++ bookland.rb
@@ -0,0 +1,37 @@
+require File.join(File.dirname(__FILE__), 'ean_13')
+
+module Barby
+
+  #Bookland barcodes are EAN-13 barcodes with number system
+  #978 (hence "Bookland"). The data they encode is an ISBN
+  #with its check digit removed. This is a convenience class
+  #that takes an ISBN no instead of "pure" EAN-13 data.
+  class Bookland < EAN13
+
+    BOOKLAND_NUMBER_SYSTEM = '978'
+
+    attr_accessor :isbn
+
+    def initialize(isbn)
+      self.isbn = isbn
+      raise ArgumentError, 'data not valid' unless valid?
+    end
+
+    def data
+      BOOKLAND_NUMBER_SYSTEM+isbn_only
+    end
+
+    #Removes any non-digit characters, number system and check digit
+    #from ISBN, so "978-82-92526-14-9" would result in "829252614"
+    def isbn_only
+      s = isbn.gsub(/[^0-9]/, '')
+      if s.size > 10#Includes number system
+        s[3,9]
+      else#No number system, may include check digit
+        s[0,9]
+      end
+    end
+
+  end
+
+end
---
+++ spec/bookland_spec.rb
@@ -0,0 +1,61 @@
+require File.join(File.dirname(__FILE__), '..', 'bookland')
+include Barby
+
+describe Bookland do
+
+  before :each do
+    @isbn = '968-26-1240-3'
+    @code = Bookland.new(@isbn)
+  end
+
+  it "should not touch the ISBN" do
+    @code.isbn.should == @isbn
+  end
+
+  it "should have an isbn_only" do
+    @code.isbn_only.should == '968261240'
+  end
+
+  it "should have the expected data" do
+    @code.data.should == '978968261240'
+  end
+
+  it "should have the expected numbers" do
+    @code.numbers.should == [9,7,8,9,6,8,2,6,1,2,4,0]
+  end
+
+  it "should have the expected checksum" do
+    @code.checksum.should == 4
+  end
+
+  it "should raise an error when data not valid" do
+    lambda{ Bookland.new('1234') }.should raise_error(ArgumentError)
+  end
+
+end
+
+
+describe Bookland, 'ISBN conversion' do
+
+  it "should accept ISBN with number system and check digit" do
+    code = nil
+    lambda{ code = Bookland.new('978-82-92526-14-9') }.should_not raise_error
+    code.should be_valid
+    code.data.should == '978829252614'
+  end
+
+  it "should accept ISBN without number system but with check digit" do
+    code = nil
+    lambda{ code = Bookland.new('82-92526-14-9') }.should_not raise_error
+    code.should be_valid
+    code.data.should == '978829252614'
+  end
+
+  it "should accept ISBN without number system or check digit" do
+    code = nil
+    lambda{ code = Bookland.new('82-92526-14') }.should_not raise_error
+    code.should be_valid
+    code.data.should == '978829252614'
+  end
+
+end
