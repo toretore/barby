0832b4b0ff29c7a1d976735de5140abea513b2fb
Tore Darell <toredarell@gmail.com>
2008-02-28 23:07:01 +0100

    Added EAN-8 barcodes

---
+++ ean_8.rb
@@ -0,0 +1,32 @@
+require File.join(File.dirname(__FILE__), 'ean_13')
+
+module Barby
+
+  #EAN-8 is a sub-set of EAN-13, with only 7 (8) digits
+  class EAN8 < EAN13
+
+    FORMAT = /^\d{7}$/
+
+
+    def left_numbers
+      numbers[0,4]
+    end
+
+    def right_numbers
+      numbers_with_checksum[4,4]
+    end
+
+
+    #Left-hand digits are all encoded using odd parity
+    def left_parity_map
+      [:odd, :odd, :odd, :odd]
+    end
+
+
+    def valid?
+      data =~ FORMAT
+    end
+
+  end
+
+end
