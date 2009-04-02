31446ca97fa7df01241d43f33cae844f8631d6c3
Tore Darell <toredarell@gmail.com>
2009-04-02 18:34:56 +0200

    Hm, that's not quite right..
    Revert "Add full_data to Code128"
    
    This reverts commit 1b3e4e2b90ea0995eef9d58379fb5e682df1ca45.

--- lib/barby/barcode/code_128.rb
+++ lib/barby/barcode/code_128.rb
@@ -174,14 +174,9 @@ module Barby
       @data
     end
 
-    def full_data
-      @full_data
-    end
-
     #Set the data for this barcode. If the barcode changes
     #character set, an extra will be created.
     def data=(data)
-      @full_data = data
       data, *extra = data.split(/([#{CODEA+CODEB+CODEC}])/n)
       @data = data
       self.extra = extra.join unless extra.empty?
