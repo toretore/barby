a35ae847d55385b1cab1bc1adc15bc8a8651b2b0
Tore Darell <toredarell@gmail.com>
2011-01-23 18:06:39 +0100

    Add checksum

--- lib/barby/barcode/upc_supplemental.rb
+++ lib/barby/barcode/upc_supplemental.rb
@@ -4,8 +4,8 @@ module Barby
 
 
   class UPCSupplemental < Barby::Barcode1D
-    @data = nil
-    attr_reader :data
+
+    attr_accessor :data
 
     FORMAT = /^\d\d\d\d\d$/
 
@@ -14,70 +14,77 @@ module Barby
     INTER_CHAR = '01'
 
     PARITY_MAPS = {
-        0 => [:even, :even, :odd, :odd, :odd],
-        1 => [:even, :odd, :even, :odd, :odd],
-        2 => [:even, :odd, :odd, :even, :odd],
-        3 => [:even, :odd, :odd, :odd, :even],
-        4 => [:odd, :even, :even, :odd, :odd],
-        5 => [:odd, :odd, :even, :even, :odd],
-        6 => [:odd, :odd, :odd, :even, :even],
-        7 => [:odd, :even, :odd, :even, :odd],
-        8 => [:odd, :even, :odd, :odd, :even],
-        9 => [:odd, :odd, :even, :odd, :even]
-      }
+      0 => [:even, :even, :odd, :odd, :odd],
+      1 => [:even, :odd, :even, :odd, :odd],
+      2 => [:even, :odd, :odd, :even, :odd],
+      3 => [:even, :odd, :odd, :odd, :even],
+      4 => [:odd, :even, :even, :odd, :odd],
+      5 => [:odd, :odd, :even, :even, :odd],
+      6 => [:odd, :odd, :odd, :even, :even],
+      7 => [:odd, :even, :odd, :even, :odd],
+      8 => [:odd, :even, :odd, :odd, :even],
+      9 => [:odd, :odd, :even, :odd, :even]
+    }
 
-     ENCODINGS_ODD = {
-        0 => '0001101', 1 => '0011001', 2 => '0010011',
-        3 => '0111101', 4 => '0100011', 5 => '0110001',
-        6 => '0101111', 7 => '0111011', 8 => '0110111',
-        9 => '0001011'
-      }
+   ENCODINGS_ODD = {
+      0 => '0001101', 1 => '0011001', 2 => '0010011',
+      3 => '0111101', 4 => '0100011', 5 => '0110001',
+      6 => '0101111', 7 => '0111011', 8 => '0110111',
+      9 => '0001011'
+    }
 
-      ENCODINGS_EVEN = {
-        0 => '0100111', 1 => '0110011', 2 => '0011011',
-        3 => '0100001', 4 => '0011101', 5 => '0111001',
-        6 => '0000101', 7 => '0010001', 8 => '0001001',
-        9 => '0010111'
-      }
+    ENCODINGS_EVEN = {
+      0 => '0100111', 1 => '0110011', 2 => '0011011',
+      3 => '0100001', 4 => '0011101', 5 => '0111001',
+      6 => '0000101', 7 => '0010001', 8 => '0001001',
+      9 => '0010111'
+    }
 
-
-
-    def data= data
-      @data = data
-    end
 
     def initialize data
       self.data = data
     end
 
+    def odd_digits
+      alternater = false
+      data.split(//).reverse.map{|c| c.to_i }.partition{ alternater = !alternater }[0]
+    end
+
+    def even_digits
+      alternater = false
+      data.split(//).reverse.map{|c| c.to_i }.partition{ alternater = !alternater }[1]
+    end
+
+    def odd_sum
+      odd_digits.inject(0){|s,d| s + d * 3 }
+    end
+
+    def even_sum
+      even_digits.inject(0){|s,d| s + d * 9 }
+    end
+
+    def checksum
+      (odd_sum + even_sum) % 10
+    end
+
     def encoding
-      begin
-        data = @data
-        return nil if data.length!=5
-        sum_odd = data[0].chr.to_i + data[2].chr.to_i + data[4].chr.to_i
-        sum_even = data[1].chr.to_i + data[3].chr.to_i
-        total = sum_odd * 3 + sum_even * 9
-        check_digit = total.modulo(10)
-       
-        parity_map = PARITY_MAPS[check_digit]
+      check_digit = checksum
 
-        pos = 0
-        e = START
-        data_chars = data.split('')
-        for digit_char in data_chars
-          digit = digit_char.to_i
-          parity = parity_map[pos]
-          e +=  ENCODINGS_ODD[digit] if parity==:odd
-          e +=  ENCODINGS_EVEN[digit] if parity==:even
-          e += INTER_CHAR unless pos == 4
-          pos += 1
-        end
+      parity_map = PARITY_MAPS[check_digit]
 
-
-        return e
-      
-      rescue
+      pos = 0
+      e = START
+      data_chars = data.split('')
+      for digit_char in data_chars
+        digit = digit_char.to_i
+        parity = parity_map[pos]
+        e +=  ENCODINGS_ODD[digit] if parity==:odd
+        e +=  ENCODINGS_EVEN[digit] if parity==:even
+        e += INTER_CHAR unless pos == 4
+        pos += 1
       end
+
+      e
     end
 
     def two_dimensional?
--- spec/upc_supplemental.rb
+++ spec/upc_supplemental.rb
@@ -30,3 +30,61 @@ describe UPCSupplemental, 'validity' do
   end
 
 end
+
+
+describe UPCSupplemental, 'checksum' do
+
+  it 'should have the expected odd_digits' do
+    UPCSupplemental.new('51234').odd_digits.should == [4,2,5]
+    UPCSupplemental.new('54321').odd_digits.should == [1,3,5]
+    UPCSupplemental.new('99990').odd_digits.should == [0,9,9]
+  end
+
+  it 'should have the expected even_digits' do
+    UPCSupplemental.new('51234').even_digits.should == [3,1]
+    UPCSupplemental.new('54321').even_digits.should == [2,4]
+    UPCSupplemental.new('99990').even_digits.should == [9,9]
+  end
+
+  it 'should have the expected odd and even sums' do
+    UPCSupplemental.new('51234').odd_sum.should == 33
+    UPCSupplemental.new('54321').odd_sum.should == 27
+    UPCSupplemental.new('99990').odd_sum.should == 54
+
+    UPCSupplemental.new('51234').even_sum.should == 36
+    UPCSupplemental.new('54321').even_sum.should == 54
+    UPCSupplemental.new('99990').even_sum.should == 162
+  end
+
+  it 'should have the expected checksum' do
+    UPCSupplemental.new('51234').checksum.should == 9
+    UPCSupplemental.new('54321').checksum.should == 1
+    UPCSupplemental.new('99990').checksum.should == 6
+  end
+
+end
+
+
+describe UPCSupplemental, 'encoding' do
+
+  before :each do
+    @data = '51234'
+    @code = UPCSupplemental.new(@data)
+  end
+
+  it 'should have the expected encoding' do
+    #                        START 5          1          2          3          4
+    UPCSupplemental.new('51234').encoding.should == '1011 0110001 01 0011001 01 0011011 01 0111101 01 0011101'.tr(' ', '')
+    #                              9          9          9          9          0
+    UPCSupplemental.new('99990').encoding.should == '1011 0001011 01 0001011 01 0001011 01 0010111 01 0100111'.tr(' ', '')
+  end
+
+  it 'should be able to change its data' do
+    prev_encoding = @code.encoding
+    @code.data = '99990'
+    @code.encoding.should_not == prev_encoding
+    #                              9          9          9          9          0
+    @code.encoding.should == '1011 0001011 01 0001011 01 0001011 01 0010111 01 0100111'.tr(' ', '')
+  end
+
+end
