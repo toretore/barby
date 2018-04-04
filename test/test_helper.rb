8d493d773f0dfadc21ef6700f9592464c1aa66af
Bernie Chiu <bernie@shoplineapp.com>
2018-04-04 20:19:54 +0800

    Provide development dependency to easily setup test run for gem maintainers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,11 +13,12 @@ module Barby
     # So we can register outputter during an full test suite run.
     def load_outputter(outputter)
       @loaded_outputter ||= load "barby/outputter/#{outputter}_outputter.rb"
+    rescue LoadError => e
+      skip "#{outputter} not being installed properly"
     end
 
     def ruby_19_or_greater?
       RUBY_VERSION >= '1.9'
     end
-
   end
 end
